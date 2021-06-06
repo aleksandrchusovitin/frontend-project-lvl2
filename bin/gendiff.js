315c6419449019eab0d032ac99da0837f7fd0c19
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-06-06 21:42:14 +0700

    update bin/gendiff.js. Add arguments and option -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import { Command } from 'commander';
 const program = new Command();
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 program.parse();
