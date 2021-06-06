04416aa6f11687c5b70f333c14811933b8c5ce52
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-06-06 21:33:22 +0700

    add command help to bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
 
-console.log('hello from gendiff.js');
+const program = new Command();
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.');
+program.parse();
