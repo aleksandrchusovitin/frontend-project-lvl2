cd109b15e57c0907aa1fd169342fcd191a62d395
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-06-07 11:49:45 +0700

    update bin/gendiff.js. Fix import and add .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../index';
 
 const program = new Command();
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
-program.parse();
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2);
+  });
+program.parse(process.argv);
