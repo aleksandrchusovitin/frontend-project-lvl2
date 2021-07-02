fa370185726ecd75d50d9753c68b3f8f8cd073f2
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-02 21:47:21 +0700

    update __tests__/diff.test.js. Renema filepath to file

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -11,8 +11,8 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const jsonFile1 = getFixturePath('file1.json');
 const jsonFile2 = getFixturePath('file2.json');
 
-const yamlFile1 = getFixturePath('filepath1.yml');
-const yamlFile2 = getFixturePath('filepath2.yml');
+const yamlFile1 = getFixturePath('file1.yml');
+const yamlFile2 = getFixturePath('file2.yml');
 
 const correctLine = `{
   - follow: false
--- src/diff.js
+++ src/diff.js
@@ -1,28 +1,9 @@
-import * as fs from 'fs';
-import yaml from 'js-yaml';
-// import * as path from 'path';
 import _ from 'lodash';
-
-const normalizeFiles = (file1, file2) => {
-  if (file1.endsWith('.json') && file2.endsWith('.json')) {
-    const normalizeFile1 = JSON.parse(fs.readFileSync(file1, 'utf8'));
-    const normalizeFile2 = JSON.parse(fs.readFileSync(file2, 'utf8'));
-
-    return { normalizeFile1, normalizeFile2 };
-  }
-
-  if ((file1.endsWith('.yml') || file1.endsWith('.yaml')) && (file2.endsWith('.yml') || file2.endsWith('.yaml'))) {
-    const normalizeFile1 = yaml.load(fs.readFileSync(file1, 'utf8'));
-    const normalizeFile2 = yaml.load(fs.readFileSync(file2, 'utf8'));
-
-    return { normalizeFile1, normalizeFile2 };
-  }
-
-  return null;
-};
+import normalizeFile from './parsers.js';
 
 export default (filepath1, filepath2) => {
-  const { normalizeFile1, normalizeFile2 } = normalizeFiles(filepath1, filepath2);
+  const normalizeFile1 = normalizeFile(filepath1);
+  const normalizeFile2 = normalizeFile(filepath2);
 
   const keys1 = _.keys(normalizeFile1);
   const keys2 = _.keys(normalizeFile2);
