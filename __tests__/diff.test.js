38163e64719fd4fa46edcb3fdac3128db1ce149f
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-04 20:41:19 +0700

    update __tests__/diff.test.js. outputted the correct string in file

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import * as path from 'path';
+import * as fs from 'fs';
 import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -14,14 +15,8 @@ const jsonFile2 = getFixturePath('file2.json');
 const yamlFile1 = getFixturePath('file1.yml');
 const yamlFile2 = getFixturePath('file2.yml');
 
-const correctLine = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const correctLineFixturePath = getFixturePath('differences.txt');
+const correctLine = fs.readFileSync(correctLineFixturePath, 'utf-8');
 
 test('gendiff test', () => {
   expect(genDiff(jsonFile1, jsonFile2)).toBe(correctLine);
