643818604dd537e13ade2d3475ef72164d21a659
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-20 18:47:07 +0700

    remove and rename src/stylish.js to src/formatters/formatStylish.js

---
+++ src/formatters/formatStylish.js
@@ -0,0 +1,53 @@
+import _ from 'lodash';
+
+const getIndent = (n) => ' '.repeat(n);
+
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return data;
+  }
+  const lines = Object
+    .entries(data)
+    .map(([key, value]) => {
+      if (_.isObject(value)) {
+        return `${getIndent(depth + 8)}${key}: ${stringify(value, depth + 4)}`;
+      }
+      return `${getIndent(depth + 8)}${key}: ${value}`;
+    });
+
+  return [
+    '{',
+    ...lines,
+    `${getIndent(depth + 4)}}`,
+  ].join('\n');
+};
+
+export default (tree) => {
+  const iter = (currentValue, depth) => {
+    const lines = currentValue.map((node) => {
+      const {
+        key, status, value, oldValue, newValue, children,
+      } = node;
+      switch (status) {
+        case 'added':
+          return `${getIndent(depth + 2)}+ ${key}: ${stringify(value, depth)}`;
+        case 'deleted':
+          return `${getIndent(depth + 2)}- ${key}: ${stringify(value, depth)}`;
+        case 'changed':
+          return `${getIndent(depth + 2)}- ${key}: ${stringify(oldValue, depth)}\n${getIndent(depth + 2)}+ ${key}: ${stringify(newValue, depth)}`;
+        case 'unchanged':
+          return `${getIndent(depth + 2)}  ${key}: ${stringify(value, depth)}`;
+        case 'hasChildren':
+          return `${getIndent(depth + 2)}  ${key}: ${iter(children, depth + 4)}`;
+        default:
+          throw new Error(`Wrong status ${status}`);
+      }
+    });
+    return [
+      '{',
+      ...lines,
+      `${getIndent(depth)}}`,
+    ].join('\n');
+  };
+  return iter(tree, 0);
+};
