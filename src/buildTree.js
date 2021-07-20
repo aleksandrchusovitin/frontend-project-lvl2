4d1a786d2cc74b623529a4d790dcf1f567a87ffc
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-20 18:45:58 +0700

    rename src/treeBuilder to src/buildTree

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const treeBuilder = (data1, data2) => {
+const buildTree = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
   const keys = _.sortBy(_.union(keys1, keys2));
@@ -17,7 +17,7 @@ const treeBuilder = (data1, data2) => {
     }
 
     if (_.isObject(value1) && _.isObject(value2)) {
-      return { key, status: 'hasChildren', children: treeBuilder(value1, value2) };
+      return { key, status: 'hasChildren', children: buildTree(value1, value2) };
     }
 
     if (value1 !== value2) {
@@ -29,4 +29,4 @@ const treeBuilder = (data1, data2) => {
   });
 };
 
-export default treeBuilder;
+export default buildTree;
