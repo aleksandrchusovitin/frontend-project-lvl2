35f2ddcee4d01e49161c3c9472ceb7d44a3c974c
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-20 18:51:01 +0700

    update bin/gendiff.js. Add default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,11 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    const { format } = program.opts();
+    console.log(format);
+    const diff = genDiff(filepath1, filepath2, format);
+    console.log(diff);
   });
 program.parse(process.argv);
