55944efa6cfd3e7913872b49b3222c07a3e64c02
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-22 16:11:07 +0700

    update src/formatters/formatPlain.js.Filtered the array by "type" BEFORE map

--- src/formatters/formatPlain.js
+++ src/formatters/formatPlain.js
@@ -8,27 +8,26 @@ const stringify = (data) => {
 
 export default (tree) => {
   const iter = (currentValue, path) => {
-    const lines = currentValue.map((node) => {
-      const {
-        key, type, value, oldValue, newValue, children,
-      } = node;
-      const newPath = [path, key].flat();
-      switch (type) {
-        case 'added':
-          return `Property '${newPath.join('.')}' was added with value: ${stringify(value)}`;
-        case 'deleted':
-          return `Property '${newPath.join('.')}' was removed`;
-        case 'changed':
-          return `Property '${newPath.join('.')}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`;
-        case 'unchanged':
-          return 'unchanged node';
-        case 'hasChildren':
-          return `${iter(children, newPath.join('.'))}`;
-        default:
-          throw new Error(`Wrong type ${type}`);
-      }
-    })
-      .filter((line) => line !== 'unchanged node');
+    const lines = currentValue
+      .filter(({ type }) => type !== 'unchanged')
+      .map((node) => {
+        const {
+          key, type, value, oldValue, newValue, children,
+        } = node;
+        const newPath = [path, key].flat();
+        switch (type) {
+          case 'added':
+            return `Property '${newPath.join('.')}' was added with value: ${stringify(value)}`;
+          case 'deleted':
+            return `Property '${newPath.join('.')}' was removed`;
+          case 'changed':
+            return `Property '${newPath.join('.')}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`;
+          case 'hasChildren':
+            return `${iter(children, newPath.join('.'))}`;
+          default:
+            throw new Error(`Wrong type ${type}`);
+        }
+      });
     return [...lines].join('\n');
   };
   return iter(tree, []);
