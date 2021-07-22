8030431ed7a2f4e9e474ade192eb2b38e19a7f98
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-22 21:48:24 +0700

    update src/formatters/formatStylish.js.Reduced the number of magic numbers

--- src/formatters/formatStylish.js
+++ src/formatters/formatStylish.js
@@ -1,5 +1,7 @@
 const makeIndent = (n) => ' '.repeat(n);
 
+const indentSize = 2;
+
 const stringify = (data, depth) => {
   if (typeof data !== 'object' || data === null) {
     return data;
@@ -8,15 +10,15 @@ const stringify = (data, depth) => {
     .entries(data)
     .map(([key, value]) => {
       if (typeof value === 'object') {
-        return `${makeIndent(depth + 8)}${key}: ${stringify(value, depth + 4)}`;
+        return `${makeIndent(depth + indentSize * 4)}${key}: ${stringify(value, depth + indentSize * 2)}`;
       }
-      return `${makeIndent(depth + 8)}${key}: ${value}`;
+      return `${makeIndent(depth + indentSize * 4)}${key}: ${value}`;
     });
 
   return [
     '{',
     ...lines,
-    `${makeIndent(depth + 4)}}`,
+    `${makeIndent(depth + indentSize * 2)}}`,
   ].join('\n');
 };
 
@@ -28,15 +30,15 @@ export default (tree) => {
       } = node;
       switch (type) {
         case 'added':
-          return `${makeIndent(depth + 2)}+ ${key}: ${stringify(value, depth)}`;
+          return `${makeIndent(depth + indentSize)}+ ${key}: ${stringify(value, depth)}`;
         case 'deleted':
-          return `${makeIndent(depth + 2)}- ${key}: ${stringify(value, depth)}`;
+          return `${makeIndent(depth + indentSize)}- ${key}: ${stringify(value, depth)}`;
         case 'changed':
-          return `${makeIndent(depth + 2)}- ${key}: ${stringify(oldValue, depth)}\n${makeIndent(depth + 2)}+ ${key}: ${stringify(newValue, depth)}`;
+          return `${makeIndent(depth + indentSize)}- ${key}: ${stringify(oldValue, depth)}\n${makeIndent(depth + indentSize)}+ ${key}: ${stringify(newValue, depth)}`;
         case 'unchanged':
-          return `${makeIndent(depth + 2)}  ${key}: ${stringify(value, depth)}`;
+          return `${makeIndent(depth + indentSize)}  ${key}: ${stringify(value, depth)}`;
         case 'hasChildren':
-          return `${makeIndent(depth + 2)}  ${key}: ${iter(children, depth + 4)}`;
+          return `${makeIndent(depth + indentSize)}  ${key}: ${iter(children, depth + indentSize * 2)}`;
         default:
           throw new Error(`Wrong type ${type}`);
       }
