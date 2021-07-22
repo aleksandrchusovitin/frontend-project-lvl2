7f528a7287f961445a39f42889aa2db81cc48402
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-22 16:13:58 +0700

    update src/formatters/formatPlain.js.Refactoring

--- src/formatters/formatPlain.js
+++ src/formatters/formatPlain.js
@@ -7,28 +7,25 @@ const stringify = (data) => {
 };
 
 export default (tree) => {
-  const iter = (currentValue, path) => {
-    const lines = currentValue
-      .filter(({ type }) => type !== 'unchanged')
-      .map((node) => {
-        const {
-          key, type, value, oldValue, newValue, children,
-        } = node;
-        const newPath = [path, key].flat();
-        switch (type) {
-          case 'added':
-            return `Property '${newPath.join('.')}' was added with value: ${stringify(value)}`;
-          case 'deleted':
-            return `Property '${newPath.join('.')}' was removed`;
-          case 'changed':
-            return `Property '${newPath.join('.')}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`;
-          case 'hasChildren':
-            return `${iter(children, newPath.join('.'))}`;
-          default:
-            throw new Error(`Wrong type ${type}`);
-        }
-      });
-    return [...lines].join('\n');
-  };
+  const iter = (currentValue, path) => currentValue
+    .filter(({ type }) => type !== 'unchanged')
+    .map((node) => {
+      const {
+        key, type, value, oldValue, newValue, children,
+      } = node;
+      const newPath = [path, key].flat();
+      switch (type) {
+        case 'added':
+          return `Property '${newPath.join('.')}' was added with value: ${stringify(value)}`;
+        case 'deleted':
+          return `Property '${newPath.join('.')}' was removed`;
+        case 'changed':
+          return `Property '${newPath.join('.')}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`;
+        case 'hasChildren':
+          return `${iter(children, newPath.join('.'))}`;
+        default:
+          throw new Error(`Wrong type ${type}`);
+      }
+    }).join('\n');
   return iter(tree, []);
 };
