2acf90043c9b278842b8daf66d9e03ad3db3104c
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-25 21:26:23 +0700

    update src/index.js.moved the logic for determining the file format into a separate function

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import parse from './parsers.js';
 import buildTree from './buildTree.js';
 import format from './formatters/index.js';
 
-export default (path1, path2, formatName = 'stylish') => {
-  const formatFile1 = path.extname(path1).slice(1);
-  const formatFile2 = path.extname(path2).slice(1);
+const determineFileFormat = (pathFile) => path.extname(pathFile).slice(1);
 
+export default (path1, path2, formatName = 'stylish') => {
   const file1 = fs.readFileSync(path1);
   const file2 = fs.readFileSync(path2);
-  const data1 = parse(file1, formatFile1);
-  const data2 = parse(file2, formatFile2);
+  const data1 = parse(file1, determineFileFormat(path1));
+  const data2 = parse(file2, determineFileFormat(path2));
 
   const tree = buildTree(data1, data2);
   return format(tree, formatName);
