3b83d0d7456086b340e7eeeaeb867daeae051d10
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-07-27 16:12:01 +0700

    update formatStylish.js. a little refactoring

--- src/formatters/formatStylish.js
+++ src/formatters/formatStylish.js
@@ -3,33 +3,36 @@ import _ from 'lodash';
 const makeIndent = (n) => ' '.repeat(n);
 
 const indentSize = 2;
+const baseIndent = 4;
+const baseCloseIndent = 2;
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return data;
   }
+  const currentIndent = depth + indentSize * baseIndent;
+  const closeIndent = indentSize * baseCloseIndent;
   const lines = Object
     .entries(data)
     .map(([key, value]) => {
       if (_.isPlainObject(value)) {
-        return `${makeIndent(depth + indentSize * 4)}${key}: ${stringify(value, depth + indentSize * 2)}`;
+        return `${makeIndent(currentIndent)}${key}: ${stringify(value, depth + closeIndent)}`;
       }
-      return `${makeIndent(depth + indentSize * 4)}${key}: ${value}`;
+      return `${makeIndent(currentIndent)}${key}: ${value}`;
     });
 
   return [
     '{',
     ...lines,
-    `${makeIndent(depth + indentSize * 2)}}`,
+    `${makeIndent(depth + closeIndent)}}`,
   ].join('\n');
 };
 
 export default (tree) => {
   const iter = (currentValue, depth) => {
-    const lines = currentValue.map((node) => {
-      const {
-        key, type, value, oldValue, newValue, children,
-      } = node;
+    const lines = currentValue.map(({
+      key, type, value, oldValue, newValue, children,
+    }) => {
       switch (type) {
         case 'added':
           return `${makeIndent(depth + indentSize)}+ ${key}: ${stringify(value, depth)}`;
